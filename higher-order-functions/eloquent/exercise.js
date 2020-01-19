f9aacd7960a96831da089bd9733a8a9fea34b9f8
reettik <reettikgoswami1997@gmail.com>
2020-01-19 18:09:29 +0530

    HOF eloquent exercise done

--- higher-order-functions/eloquent/exercise.js
+++ higher-order-functions/eloquent/exercise.js
@@ -2,20 +2,46 @@
 let arrays = [[1, 2, 3], [4, 5], [6]];
 
 // Your code here.
-// → [1, 2, 3, 4, 5, 6]
 
+function singleArray(arrays){
+  arrays.reduce((newArray ,array ) =>{
+   array.forEach(cv => newArray.push(cv));
+    return newArray;
+ },newArray = []);
+ return newArray;
+}
+
+console.log(singleArray(arrays));
+// → [1, 2, 3, 4, 5, 6]
+//========================================================================
 // Challenge 2. Your own loop
 // Your code here.
+function loop(initValue , terminatingCondition , increment  , operation){
+  let i = initValue; 
+  while(terminatingCondition(i)){
+    operation(i);
+    i = increment(i);
+  }
+}
+
 
 loop(3, n => n > 0, n => n - 1, console.log);
 // → 3
 // → 2
 // → 1
 
+//===========================================================================
 // Challenge 3. Everything
+
 function every(array, test) {
-  // Your code here.
+  
+  let bool = true ;
+  for(let i = 0;i<array.length;i++){
+   bool = test(array[i]) & bool;
+  }
+  return Boolean(bool);
 }
+
 
 console.log(every([1, 3, 5], n => n < 10));
 // → true
@@ -24,6 +50,7 @@ console.log(every([2, 4, 16], n => n < 10));
 console.log(every([], n => n < 10));
 // → true
 
+//==========================================================================
 // Challenge 4. Dominant writing direction
 function dominantDirection(text) {
   // Your code here.
